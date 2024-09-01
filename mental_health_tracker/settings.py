ddbe9c710c9bbdd94e00435da465ba2c472d8a53
kirbywoo <eva.yunia110904@gmail.com>
2024-09-01 19:19:18 +0700

    add url deployment PWS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&@b1z$kx=#bbs3rs(lmkq&zsy03cn)t_o(-**8^@4@%+hi!cku
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "eva-yunia-mentalhealthtracker.pbp.cs.ui.id"]
 
 
 # Application definition
