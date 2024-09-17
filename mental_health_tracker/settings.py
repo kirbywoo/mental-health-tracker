e0db752afc6287aaa59eae64be3cd70c517dec39
kirbywoo <eva.yunia110904@gmail.com>
2024-09-18 01:32:18 +0700

    update pws url

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&@b1z$kx=#bbs3rs(lmkq&zsy03cn)t_o(-**8^@4@%+hi!cku
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "eva-yunia-mentalhealthtracker.pbp.cs.ui.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "eva-yunia-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
